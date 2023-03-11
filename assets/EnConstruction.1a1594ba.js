4cf01debfab6dc511d11478d8024c3752c529cad
Kourylles <sitemarchand@outlook.fr>
2023-03-11 23:10:02 +0100

    Modification publicPath

---
+++ assets/EnConstruction.1a1594ba.js
@@ -0,0 +1,1 @@
+import{_ as n,J as e,U as c,$ as o,K as r}from"./index.24245956.js";const a={},_=o("p",{class:"text-h4 text-light-brown"}," Cette page n'est pas encore disponible ",-1),i=[_];function p(t,s){return e(),c("div",null,i)}var l=n(a,[["render",p]]);const f={__name:"EnConstruction",setup(t){return(s,d)=>(e(),r(l))}};export{f as default};
